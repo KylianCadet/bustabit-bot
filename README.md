05a260a6285ce8c206f1a9b264e20ff58bb1d5db
kylian <kylian.cadet@epitech.eu>
2020-03-19 17:21:34 +0100

    add base bot -> able to launch to flat bet stratgie in simated mode 	new file:   .gitignore 	modified:   README.md 	new file:   bot.py

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# bustabit-bot
+# Bustabit Bot
+
+Bustabit Bot is a solution to run the bustabit game (https://www.bustabit.com/) without using a web browser directly
+
+### How it works
+
+> Bustabit does not offer an API to work with, the best workaround is to simulate a web browser activty.
+>*Selenium* (https://www.selenium.dev/) is a powerfull api to automates browser activty.
+
+We are simulating the creation of a new bot in the bustabit format (https://github.com/bustabit/autobet) and we launch it.
+
+### How to setup (Only Firefox)
+
+  - Loggin to Bustabit with your Firefox account
+  - Get your Firefox profile folder (https://support.mozilla.org/en-US/kb/profiles-where-firefox-stores-user-data)
+  - Copy your folder next to the bot.py
+  
+
+### How to launch
+```sh
+$ python3 bot.py [firefox_folder]
+```
---
+++ bot.py
@@ -0,0 +1,100 @@
+import time
+import signal
+from selenium import webdriver
+import selenium.webdriver.support.ui as ui
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import sys
+
+class Bustabit:
+    _browser = None
+    _error = False
+
+    def __init__(self):
+        # Launch Firefox GUI
+        self._browser = webdriver.Firefox("./firefox_profile")
+
+        # Catch SIGINT to close browser before exiting
+        def signal_handler(signal, frame):
+            self._browser.quit()
+            exit(0)
+
+        signal.signal(signal.SIGINT, signal_handler)
+        return
+
+    def _login(self):
+        """Login into bustabit by getting the login pages"""
+        self._browser.get('https://www.bustabit.com/login')
+
+        # Get username and password textboxes
+        user_textbox = self._browser.find_element_by_name("uname")
+        password_textbox = self._browser.find_element_by_name("password")
+
+        # Fill userame and password textboxes
+        user_textbox.send_keys(self._username)
+        password_textbox.send_keys(self._password)
+
+        # Click on submit button
+        sumbit_button = self._browser.find_element_by_class_name("btn-lg")
+        sumbit_button.click()
+        return
+
+    def _connect(self):
+        """Init browser"""
+        self._browser.get('https://www.bustabit.com/play')
+
+        # Wait until we find the presence of tha 'auto' button
+        try:
+            wait = ui.WebDriverWait(self._browser, 5)
+            wait.until(EC.presence_of_element_located((By.XPATH, "//li[@class='' and @role='presentation']/a[@role='button' and @href='#']")))
+        except:
+            self._error = True
+        return
+
+    def _auto_bet(self):
+        """Starting auto bet with flat bet strategie in simulated mode"""
+
+        # Get and click on 'Auto' button
+        auto_button = self._browser.find_element_by_xpath("//li[@class='' and @role='presentation']/a[@role='button' and @href='#']")
+        auto_button.click()
+
+        # Get and click on 'Flat bet' arrow button
+        flat_bet_button = self._browser.find_element_by_xpath("//button[@class='btn btn-xs btn-default']")
+        flat_bet_button.click()
+
+        # Get and click on 'Simulation' checkbox
+        simulation_button = self._browser.find_element_by_xpath("//div[@class='checkbox simCheckbox']/label/input[@type='checkbox']")
+        simulation_button.click()
+
+        # Get and fill the 'simulated balance'
+        SIMULATED_BALANCE = 100000
+        simulated_balance_textbox = self._browser.find_element_by_name("simulatedBalance")
+        simulated_balance_textbox.clear()
+        simulated_balance_textbox.send_keys(str(SIMULATED_BALANCE))
+
+        # Get and click on the 'Run script' button
+        run_script_button = self._browser.find_element_by_xpath("//button[@class='btn btn-success' and @type='submit']")
+        run_script_button.click()
+        return
+
+    def _run(self):
+        """Infinite loop"""
+        while True:
+            time.sleep(1)
+
+    def start(self):
+        """Start the Bustabit bot"""
+        self._connect()
+        if (self._error):
+            print('Are you sure you are logged with your profile ?')
+            self._browser.quit()
+            return
+        self._auto_bet()
+        self._run()
+        return
+
+
+if __name__ == "__main__":
+    bot = Bustabit()
+    bot.start()
+    exit(0)
